b264fce6270cec30b6972043ea269fd3c42240f0
Silvina <silprado96@gmail.com>
2022-08-31 11:55:47 -0300

    dowhile

--- java/leccionsemestre2/CicloWhile/src/CicloWhile/EjercicioWhile01.java
+++ java/leccionsemestre2/CicloWhile/src/CicloWhile/EjercicioWhile01.java
@@ -4,9 +4,15 @@ package CicloWhile;
 public class EjercicioWhile01 {
     public static void main(String[] args) {
         var conteo = 0; //inferencia de tipos
-        while (conteo < 7) {
+        while (conteo < 3) {
             System.out.println("conteo = " + conteo);
             conteo++; //vamos aumentando en uno la var
         }
+        
+        var contador = 0;
+        do{
+            System.out.println("contador = " + contador);
+            contador++;
+        }while(contador <= 7);
     }
 }
