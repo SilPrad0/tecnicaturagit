6bebb5c71595f72c386c7fc2681dcac27ec8c188
Silvina <silprado96@gmail.com>
2022-11-09 11:21:56 -0300

    JS, 02 y 03
    Clases 6  y 7

--- Javascript/leccion2/02-01-ConcatenaciondeCadenas.js
+++ Javascript/leccion2/02-01-ConcatenaciondeCadenas.js
@@ -1,5 +1,5 @@
 var nombre = "silvina";
-var apellido = "perez";
+var apellido = "prado";
 var nombreCompleto = nombre+" "+apellido;
 console.log(nombreCompleto)
 var nombreCompleto2 = "jose"+" "+"perez";
@@ -10,5 +10,19 @@ juntos = nombre + 435 + 14;
 console.log(juntos)
 juntos = 12 + 13 + "silvina";
 console.log(juntos)
+//usando el operador simplificado concateno str tmb
 nombre += apellido;
 console.log(nombre);
+
+//hoy ya no se usa var, si let y const
+
+let nombre2 = "messi";
+console.log(nombre2);
+
+const apellido2 = "messsi";
+console.log(apellido2);
+
+let x, y; //se pueden crear varias variables en una sola linea
+x = 17, y = 12; //se las puede asignar en una sola linea tmb
+let z = x + y;
+console.log(z);
